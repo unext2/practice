595678d067b46a24899e438d9f41e6cb816fbcb7
Petrichenko Ivan <unext1@mail.ru>
2024-04-16 11:02:45 +0300

    feat: update CreateSql script and add schema

--- 1.DB/CreateScript.sql
+++ 1.DB/CreateScript.sql
@@ -1,61 +1,43 @@
 CREATE TABLE GENRES (
-	id UUID PRIMARY KEY,
+	id LONG PRIMARY KEY,
 	name VARCHAR(50)
 );
 
 CREATE TABLE ALBUMS (
-	id UUID PRIMARY KEY,
+	id LONG PRIMARY KEY,
 	title VARCHAR(50),
-	date DATE
 );
 
 CREATE TABLE ARTISTS (
-	nickname VARCHAR(50) PRIMARY KEY,
+	id LONG PRIMARY KEY,
 	name VARCHAR(50)
 );
 
 CREATE TABLE EMPLOYEES (
-	passport_id UUID PRIMARY KEY,
+	id LONG PRIMARY KEY,
 	name VARCHAR(50)
 );
 
-CREATE TABLE EQUIPMENT (
-	name VARCHAR(50) PRIMARY KEY,
-	kind VARCHAR(50)
-	
-);
-
 CREATE TABLE SONGS (
-	id UUID PRIMARY KEY,
-	album_id UUID,
-	number integer,
-	title VARCHAR(50),
-	FOREIGN KEY (album_id) REFERENCES ALBUMS (id)
-);
-
-CREATE TABLE DISCOGRAPHY (
-	id UUID PRIMARY KEY,
-	artist_nickname VARCHAR(50),
-	album_id UUID,
-	FOREIGN KEY (artist_nickname) REFERENCES ARTISTS (nickname),
+	id LONG PRIMARY KEY,
+	album_id LONG,
+	name VARCHAR(50),
 	FOREIGN KEY (album_id) REFERENCES ALBUMS (id)
 );
 
 CREATE TABLE RECORDS (
-	id UUID PRIMARY KEY,
-	artist_nickname VARCHAR(50),
-	song_id UUID,
-	employee_passport_id UUID,
-	equipment_name VARCHAR(50),
-	FOREIGN KEY (artist_nickname) REFERENCES ARTISTS (nickname),
+	id LONG PRIMARY KEY,
+	artist_id LONG,
+	song_id LONG,
+	employee_id LONG,
+	date DATE,
+	FOREIGN KEY (artist_id) REFERENCES ARTISTS (id),
 	FOREIGN KEY (song_id) REFERENCES SONGS (id),
-	FOREIGN KEY (employee_passport_id) REFERENCES EMPLOYEES (passport_id),
-	FOREIGN KEY (equipment_name) REFERENCES EQUIPMENT (name)
+	FOREIGN KEY (employee_id) REFERENCES EMPLOYEES (id)
 );
- CREATE TABLE ALBUM_STYLE (
- 	 id UUID PRIMARY KEY,
-	 album_id UUID,
-	 genre_id UUID,
+ CREATE TABLE ALBUM_GENRE (
+	 album_id LONG,
+	 genre_id LONG,
 	 FOREIGN KEY (album_id) REFERENCES ALBUMS (id),
 	 FOREIGN KEY (genre_id) REFERENCES GENRES (id)
  );
